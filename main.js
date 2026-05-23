0e568c43d80522131b83bdb63e5456618de9777e
labyrt <labyrtcontact@gmail.com>
2026-05-23 08:34:09 -0300

    Primeiro commit

---
+++ main.js
@@ -0,0 +1,8 @@
+const form = document.getElementById('form-atividade');
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    const inputNomeAtividade = document.getElementById('nome-atividade');
+    const inputNotaAtividade = document.getElementById('nota-atividade');
+
+    alert(`Atividade: ${inputNomeAtividade.value} - Nota: ${inputNotaAtividade.value}`);
